1c0ab6c17b059ac9c9fb308f4c0857142420a325
SimonMachhabhai <Simon_96m@hotmail.com>
2020-08-28 15:51:58 +0200

    Words(not done)

---
+++ words/Words/WordsApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skriv en konsolapp som tar emot en skriven text.
+
+            //Vi vill ha ut följande
+            //Antal ord
+            //Antal vokaler 
+            //Vilket det längsta ordet är
+
+            //Word count.
+            //Vowels count.
+            //Longest word.
+
+
+            Console.WriteLine("Skriv en fin och lång mening tack: ");
+
+            char[] vowels = new char[] { 'a', 'i', 'o', 'e', 'u', 'y', 'å', 'ä', 'ö' };
+
+
+            string myTestString = Console.ReadLine();
+            string myLowercaseString = myTestString.ToLower();
+            string[] words = myTestString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+
+
+
+
+            foreach (var charatcher in myTestString)
+            {    
+                var normalizedCharachter = char.ToLower(charatcher);
+
+                if (vowels.Contains(normalizedCharachter))
+                {
+
+                }
+            }
+            for (var i = 0; i < myTestString.Length; i++)
+            {
+                var character = myTestString[i];
+            }
+
+
+
+
+
+        }
+    }
+}
